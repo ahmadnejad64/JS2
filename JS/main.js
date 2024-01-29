06ecdded3d3d902d86083e32498354d4bca7bc1d
ahmadnejad <ahmadnjad@yahoo.com>
2024-01-29 16:13:30 +0330

    first commit

---
+++ JS/main.js
@@ -0,0 +1,173 @@
+//alert ("hi world");
+console.log ("bagher");
+console.error ("خطا دادی بابا جان");
+console.warn ("این چه وضع ش");
+document.getElementById ("content").innerHTML="welcom";
+
+
+
+
+let bagher = "خوش آمدیگویی"
+
+console.log(bagher.length)
+
+
+
+const age=2.6;
+console.log (typeof age );
+
+const FirstName ="bagher ahmadneja";
+console.log (typeof FirstName );
+
+const isdon= false;
+console.log (typeof isdon );
+
+
+
+let momo =125;
+var nono= "iranin";
+
+console.log (momo +"\n "+ nono);
+
+
+const bahar="iranin";
+console.log (typeof bahar);
+
+// let nam=prompt("سن خود را کامل وارد کنید");
+// let nameage=Number(nam)
+
+// console.log(nameage)
+
+// console.log (bahar+nono+momo);
+
+
+
+// if (bahar===nono) {
+//     console.log("عملکردت خوب بود")
+    
+// }else {console.log("جالب نبود")};
+
+
+
+// let user=prompt("عدد خودتون رو وارد کنید")
+
+// if (user%2==0) {
+//     alert("عدد زوج")
+    
+// } else { alert ("عدد فرد است")
+    
+// }
+
+//const num1=+prompt ("تعداد اسپری موپ:")
+// const num2=+prompt ("تعداد موپ:")
+// const num3=+prompt ("تعداد سطل پدالی:")
+// let sumnum=num1+num2+num3
+
+
+
+// let averg= (num1+num2+num3)/3
+// console.log (averg)
+
+
+// if (num1>522 && num3>600) {
+    //     console.log ("عملکرد ضعیف بوده")
+    
+    // } else if (num2>1000) {
+        //     console.log ("بیخیال داداش")
+        
+        // }else {
+            //     console.log ("عملکرد خوب بوده")
+            
+            // }
+            
+            // num1>522?console.log ("عملکرد خوب بوده"): console.log ("عملکرد ضعیف بوده")
+            
+            //num2==1000 && num3==2000 ? console.log ("عملکرد خوب بوده"):console.log ("عملکرد ضعیف بوده")
+            
+            // switch (sumnum) {
+                //     case 1000:console.log ("عملکرد خوب بوده")
+                //     break;
+                
+                //     case 2000:console.log ("عملکرد متوسط بوده")
+                //     break;
+                
+                //     default:console.log("راضی نیستم")
+                //         break;
+                // }
+                
+function bagherr() {
+   let num5=205
+   let num6=5  
+   let num7=num5+num6               
+                
+ console.log(num7) };
+                    
+bagherr()
+                    
+//console.log(bagherr)
+
+
+var ali =function (uu,xx) {
+    var1=506
+    var2=55
+
+    console.log(var1+var2+uu+xx)
+};
+
+ali("10","20");
+bagherr();
+
+
+// function avrg (u1,u2,u3) {
+//     var summ= u1+u2+u3
+//     var avrg2=summ/3
+//     return avrg2
+// }
+
+// avrg(87,97,100);
+
+
+let avvrg =(u1,u2,u3)=> {var summ= u1+u2+u3 
+ var avrg2=summ/3
+console.log(avrg2)};
+
+avvrg(20,56,45);
+
+function mono(uuuu) { var modo = 15; console.log(modo + uuuu); }
+
+mono(50);
+
+
+for (let i = 0; i <10; i=i+3) {
+    console.log(i);
+    
+}
+
+// let allprice = 0;
+// for (let i=0 ; i<5 ; i++) {
+//     allprice=allprice+Number(prompt("قیمت را وارد کنید"))
+// }
+// alert(allprice)
+console.log("تمرین بعدی")
+let i=2
+while (i<20) {
+    console.log(i)
+    i+=+2
+}
+
+console.log("تمرین بعدی")
+
+
+do {
+    console.log(i)
+} while (i<5);
+
+console.log("تمرین بعدی")
+
+let all = [152 , 850 , 9800 , 1000]
+
+
+alert(all);
+
+confirm("تو باقری ؟");
+
